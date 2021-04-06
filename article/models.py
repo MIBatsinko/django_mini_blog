8537a61bbac43725fae9da046c030cf6a5692a46
Maksym Batsinko <batsinko.m@gmail.com>
2021-04-06 11:15:03 +0300

    Added category

--- article/models.py
+++ article/models.py
@@ -3,11 +3,24 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+    name = models.CharField("Category", max_length=150)
+    description = models.TextField("Description")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
 class Article(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     body = models.TextField()
     author = models.ForeignKey(User, related_name='article', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, default=4)
     total_rating = models.FloatField(default=0)
 
     def get_absolute_url(self):
--- blog/forms.py
+++ blog/forms.py
@@ -7,7 +7,7 @@ from .models import UserProfile, Rating, RatingStar
 class ArticlesForm(ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'description', 'body']
+        fields = ['title', 'description', 'body', 'category']
 
         widgets = {
             'title': TextInput(attrs={
