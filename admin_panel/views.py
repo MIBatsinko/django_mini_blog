b11aa77b9b58897352222c5f3021ba26d0e78754
Maksym Batsinko <batsinko.m@gmail.com>
2021-04-07 13:46:24 +0300

    Added Main Sidebar Container (user)

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from blog.models import UserProfile
+
 
 class BlogHomePage:
     def home(self):
-        return render(self, 'admin_panel/dashboard.html')
+        user_profile = UserProfile.objects.get(user=self.user)
+        return render(self, 'admin_panel/dashboard.html', {'user_profile': user_profile})
