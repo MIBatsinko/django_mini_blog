f0881478099192a291a6a1be4e38ea354cf323fc
Maksym Batsinko <batsinko.m@gmail.com>
2021-04-07 14:19:33 +0300

    Upgrade admin panel

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.BlogHomePage.home, name='blog_index'),
+    path('user_info/', views.User.info, name='user_info')
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -7,3 +7,9 @@ class BlogHomePage:
     def home(self):
         user_profile = UserProfile.objects.get(user=self.user)
         return render(self, 'admin_panel/dashboard.html', {'user_profile': user_profile})
+
+
+class User:
+    def info(self):
+        user_profile = UserProfile.objects.get(user=self.user)
+        return render(self, 'admin_panel/user_info.html', {'user_profile': user_profile})
