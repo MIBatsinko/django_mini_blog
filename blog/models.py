60fe8fc4f6eed4bed630fa85959cbdb9f372cfd6
Maksym Batsinko <batsinko.m@gmail.com>
2021-04-12 16:02:08 +0300

    Fixed

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,6 @@ from article.models import Article
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(default="/profile1.png", null=True, blank=True)  # upload_to='images/',
-    name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     total_rating = models.FloatField(default=0)
 
