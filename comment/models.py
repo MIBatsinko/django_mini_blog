921f0dda231a202664969c50c5751e8216503cd5
Maksym Batsinko <batsinko.m@gmail.com>
2021-04-13 15:46:01 +0300

    Fixed

--- comment/models.py
+++ comment/models.py
@@ -6,12 +6,10 @@ from django.contrib.auth.models import User
 
 
 class Comment(models.Model):
-    """
-
-    """
     body = models.TextField()
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def get_absolute_url(self):  # new
         return reverse('blog_view', args=[str(self.id)])
