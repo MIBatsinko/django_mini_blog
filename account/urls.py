e313974e9a2c28491857b646f0dbc8c5051804ce
Maksym Batsinko <batsinko.m@gmail.com>
2021-04-13 19:23:02 +0300

    New authorisation and registration

--- account/urls.py
+++ account/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('login/', views.user_login, name='account_login'),
     path('register/', views.register, name='account_signup'),
     path('logout/', views.LogoutView.as_view(), name='account_logout'),
-    path('', views.dashboard, name='dashboard'),
 ]
