c3da5f37c91b4972041e5001944593d5245fa6d2
Maksym Batsinko <batsinko.m@gmail.com>
2021-04-19 11:41:19 +0300

    fixed

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -39,7 +39,7 @@ class AdminUserProfileView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['select_user'] = User.objects.get_object_or_404(id=kwargs.get('pk'))
+        context['select_user'] = User.objects.get(id=kwargs.get('pk'))
         return context
 
 
@@ -51,7 +51,7 @@ class AdminUserProfileUpdateView(UpdateView):
 
     def get(self, request, *args, **kwargs):
         pk = kwargs.get('pk')
-        user = User.objects.get_object_or_404(id=pk)
+        user = User.objects.get(id=pk)
         user_profile_form = UserProfileForm(initial={
                 'avatar': user.userprofile.avatar,
             })
@@ -68,7 +68,7 @@ class AdminUserProfileUpdateView(UpdateView):
 
     def post(self, request, *args, **kwargs):
         pk = kwargs.get('pk')
-        user = User.objects.get_object_or_404(id=pk)
+        user = User.objects.get(id=pk)
         user_profile_form = UserProfileForm(self.request.POST, self.request.FILES, instance=user.userprofile)
         user_form = UserForm(self.request.POST, instance=user)
         if user_profile_form.is_valid() and user_form.is_valid():
@@ -120,7 +120,7 @@ class AdminArticleCreateView(CreateView):
         return context
 
     def form_valid(self, form):
-        category = Category.objects.get_object_or_404(name=self.request.POST.get('category'))
+        category = Category.objects.get(name=self.request.POST.get('category'))
         instance = form.save(commit=False)
         instance.author = self.request.user
         instance.category = category
@@ -221,6 +221,12 @@ class AdminCommentDeleteView(DeleteView):
     success_url = '/admin_panel/articles/'
     template_name = 'admin_panel/comments/comment_delete.html'
 
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        article_id = self.object.article.id
+        self.object.delete()
+        return redirect(reverse_lazy('article_details', (article_id,)))
+
 
 @method_decorator(decorators, name='dispatch')
 class AdminCommentCreateView(CreateView):
@@ -247,13 +253,13 @@ class AdminCommentCreateView(CreateView):
 
 class AdminUserIsActive:
     def deactivate(self, pk):
-        user_id = User.objects.get_object_or_404(id=pk)
+        user_id = User.objects.get(id=pk)
         user_id.is_active = False
         user_id.save()
         return redirect('users')
 
     def activate(self, pk):
-        user_id = User.objects.get_object_or_404(id=pk)
+        user_id = User.objects.get(id=pk)
         user_id.is_active = True
         user_id.save()
         return redirect('users')
