87640e0cfb2df758ee77a22185d2851e0a289112
Maksim Batsinko <pochta.haha@gmail.com>
2021-05-27 11:12:27 +0300

    fixed

--- my_account/views.py
+++ my_account/views.py
@@ -16,51 +16,32 @@ class LoginView(View):
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(username=cd['username'], password=cd['password'])
-            if user is not None and user.is_active:
+            if user is not None:
                 if user.is_active:
                     login(request, user)
                     return redirect('/')
                 else:
                     return HttpResponse('Disabled my_account')
             else:
-                return render(request, 'my_account/invalid_login.html')
+                return render(request, 'my_account/page-login.html', {'error': "Invalid login!", 'form': form})
+        else:
+            return render(request, 'my_account/page-login.html', {'form': form})
 
     def get(self, request):
         form = LoginForm()
         return render(request, 'my_account/page-login.html', {'form': form})
-
-#
-# def user_login(request):
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(username=cd['username'], password=cd['password'])
-#             if user is not None:
-#                 if user.is_active:
-#                     login(request, user)
-#                     return redirect('/')
-#                 else:
-#                     return HttpResponse('Disabled my_account')
-#             else:
-#                 return render(request, 'my_account/invalid_login.html')
-#     else:
-#         form = LoginForm()
-#     return render(request, 'my_account/page-login.html', {'form': form})
 
 
 class SignupView(View):
     def post(self, request):
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
-            # Set the chosen password
             new_user.set_password(user_form.cleaned_data['password'])
-            # Save the User object
             new_user.save()
 
             return render(request, 'my_account/register_done.html', {'new_user': new_user})
+        return render(request, 'my_account/page-sign-up.html', {'user_form': user_form})
 
     def get(self, request):
         user_form = UserRegistrationForm()
